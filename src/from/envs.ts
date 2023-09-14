7df3d387a1b676deaf31c3158ba1b9729e56e0ed
Jinbo Wang <jinbwan@microsoft.com>
2023-09-14 13:12:45 +0800

    Exclude the macOS preserved '/usr' from java home list (#16)

--- src/from/envs.ts
+++ src/from/envs.ts
@@ -1,7 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
-import { expandTilde, getRealHome, looksLikeJavaHome, JAVA_FILENAME } from "../utils";
+import { expandTilde, getRealHome, looksLikeJavaHome, JAVA_FILENAME, isMac } from "../utils";
 
+const EXCLUSIONS_ON_MAC: Set<string> = new Set([
+    "/usr",
+    "/usr/"
+]);
 export async function candidatesFromPath(): Promise<string[]> {
     const ret = [];
     if (process.env.PATH) {
@@ -13,7 +17,13 @@ export async function candidatesFromPath(): Promise<string[]> {
          * Fix for https://github.com/Eskibear/node-jdk-utils/issues/2
          * Homebrew creates symbolic links for each binary instead of folder.
          */
-        const homeDirs = await Promise.all(jdkBinFolderFromPath.map(p => getRealHome(path.dirname(p))));
+        const homeDirs = (await Promise.all(jdkBinFolderFromPath.map(p => getRealHome(path.dirname(p))))).filter(homeDir => {
+            /**
+             * Fix for https://github.com/Eskibear/node-jdk-utils/issues/15
+             * /usr/bin/java is a preserved shortcut program on macOS, which is not a real java home.
+             */
+            return homeDir && !(isMac && EXCLUSIONS_ON_MAC.has(homeDir));
+        });
         ret.push(...homeDirs);
     }
     return ret.filter(Boolean) as string[];
